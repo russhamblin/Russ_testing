30ffefa33d39a3af8cebf7384b4440166d975c5f
Russell hamblin <russhamblin@gmail.com>
2022-09-19 08:40:20 +1000

    updated to replace _RVT with other text

--- rename_BIM360.py
+++ rename_BIM360.py
@@ -45,54 +45,18 @@ def Files_To_Rename():
     for item in List_Of_Files_To_Rename:
         textinfo.insert((count), (item))
         count = count + 1
-    
-    
-#--------------------------------using the check button see what files need to be reviewed against----------------------
-def file_move_check():
-    global Dirpath
-    filenames = os.listdir(Dirpath)  
-    for filename in filenames:
-        filepath = os.path.basename(filename)
-        filename = os.path.splitext(filepath)[0]  
-        
-        if "_backup" in filename:                   
-            pass            
-
-        else:
-            List_of_files_to_check.append(filename)
-
-    count = 0
-    for rename in List_of_files_to_check:
-        if rename in List_Of_Files_To_Rename: 
-            pass
-        
-                      
-        else: 
-            file_info.insert((count), (rename))
-            List_Of_Files_To_Action.append(rename)
-            count = count + 1 
+  
 
 #-----------------------------------------using the run button to action the files------------------------------------------
 def run_this():
-    replaceThis = Text_TO_Replace.get()
-    WithThat = Text_To_Replace_With.get()
+
     for f in List_Of_Files_To_Rename:
         global FileExt            
-        fnew = f.split('_[')[0] 
-        fnew = fnew.replace(replaceThis, WithThat)     
-        rev_list = []        
-        for file in List_Of_Files_To_Action:                               
-            if  file.startswith(fnew):
-                rev = file.split(f'{fnew}')[1]
-                rev = rev.replace("_","")                
-                rev_list.append(rev)
-            else:
-                pass
-        rev_list.sort(reverse=True)
-        new_rev = rev_list[0]
-        update_rev = int(new_rev)
-        update_rev = update_rev+1
-        newname = f"{fnew}_{update_rev}"
+        fnew = f.split('_RVT')[0] 
+        fnew = fnew + "_RVT"     
+   
+
+        newname = f"{fnew}"
         renamer(f, newname,FileExt)
         
 
@@ -123,27 +87,27 @@ def replacetext():
 textFileButton = Button(root, text="choose Files to rename", command=Files_To_Rename)
 btn_run = Button(root, text="Run", command= run_this)
 textinfo = Listbox(root)
-file_info = Listbox(root, height= 10)
-runfilemovecheck = Button(root,text= "RUN CHECK", command= file_move_check,)
+#file_info = Listbox(root, height= 10)
+#runfilemovecheck = Button(root,text= "RUN CHECK", command= file_move_check,)
 Cancelprocess = Button(root, text = "CANCEL", command = root.destroy)
 Clear = Button(root, text="Clear ALL", command= Clear_this)
-replacethistextlabel = Label(root, text= "find text")
-replacewithtextlabel = Label(root, text= "replace")
-replace_thisText = Entry(root, textvariable = Text_TO_Replace )
-replace_thatText = Entry(root, textvariable = Text_To_Replace_With)
+#replacethistextlabel = Label(root, text= "find text")
+#replacewithtextlabel = Label(root, text= "replace")
+#replace_thisText = Entry(root, textvariable = Text_TO_Replace )
+#replace_thatText = Entry(root, textvariable = Text_To_Replace_With)
 
 #-------------------------------------where the widgits are placed--------------------------------------------
 textFileButton.grid(row=1, column=1, sticky = "ne", padx=10, pady=10)
 btn_run.grid(row=4, column=3, sticky = "ne",padx=10, pady=10)
 textinfo.grid(row=1, column=2, sticky="nsew", padx=10)
-file_info.grid(row=2, column= 2, sticky="nsew",padx=10, pady=10)
-runfilemovecheck.grid(row=2, column = 1,sticky = "ne",padx=10, pady=10)
+#file_info.grid(row=2, column= 2, sticky="nsew",padx=10, pady=10)
+#runfilemovecheck.grid(row=2, column = 1,sticky = "ne",padx=10, pady=10)
 Cancelprocess.grid(row = 3,column =2,sticky = "ne",padx=10, pady=10)
 Clear.grid(row = 6,column =2,sticky = "ne",padx=10, pady=10)
-replacethistextlabel.grid(row = 3,column =1,sticky = "ne",padx=10, pady=10)
-replacewithtextlabel.grid(row = 3,column =2,sticky = "nw",padx=10, pady=10)
-replace_thisText.grid(row = 4,column =1,sticky = "ne",padx=10, pady=10)
-replace_thatText.grid(row = 4,column =2,sticky = "nw",padx=10, pady=10)
+#replacethistextlabel.grid(row = 3,column =1,sticky = "ne",padx=10, pady=10)
+#replacewithtextlabel.grid(row = 3,column =2,sticky = "nw",padx=10, pady=10)
+#replace_thisText.grid(row = 4,column =1,sticky = "ne",padx=10, pady=10)
+#replace_thatText.grid(row = 4,column =2,sticky = "nw",padx=10, pady=10)
 
 #-------------------------------------renaming files-------------------------------------------------------
 
